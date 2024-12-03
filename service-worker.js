fbca2936c31811d189262a31a4f1f767752a2f5e
Mimi <mimi@heh.be>
2024-12-03 14:06:58 +0100

    add sw

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const version = 1.01
+
+self.addEventListener('install', event => {
+    event.waitUntil
+    console.log('SW installé' + version)
+    return self.skipWaiting()
+})
+
+self.addEventListener('activate', () => {
+    console.log('Activation SW' + version)
+
+    return self.clients.claim()
+})
